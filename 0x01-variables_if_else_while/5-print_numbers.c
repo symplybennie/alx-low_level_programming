b0865681855061e31c3d2b4f5e9b254c3d753dac
symplybennie <benedictanwa19@gmail.com>
2022-04-11 07:28:31 -0700

    a program that prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+   * main - entry point
+    *
+     * Description: Print numbers 0 t0 9 on one line
+      * Return: 0
+       */
+int main(void)
+{
+	        char m;
+		        /* Blank space after declaration */
+		        for (m = '0'; m <= '9'; ++m)
+				                putchar(m);
+
+			        putchar('\n');
+
+				        return (0);
+}
