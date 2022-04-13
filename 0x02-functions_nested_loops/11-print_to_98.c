9e9b59efdd73528ceab0442d959e2af9d30c7931
symply$bennie <46108655+symplybennie@users.noreply.github.com>
2022-04-13 17:23:15 +0100

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
-   * print_to_98 - print to 98
-    * @n : number to start from
-     * Return:0 or 1
-      */
+ * print_to_98 - print to 98
+ * @n : number to start from
+ * Return:0 or 1
+ */
 
 void print_to_98(int n)
 {
