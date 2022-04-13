4747331c6ed7fe7f2c216bb9daafb1b1c23e8277
symply$bennie <46108655+symplybennie@users.noreply.github.com>
2022-04-13 17:21:47 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * print_times_table - prints times table
- *   * @n : times table to use
- *    * Return:void
- *     */
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
+ */
 
 void print_times_table(int n)
 {
