78cee0d3d9ea0b6424f5566ab28517e7ae280fdb
symply$bennie <46108655+symplybennie@users.noreply.github.com>
2022-04-13 17:20:25 +0100

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  * main - main block
- *   * Description: computes and prints the sum of all the multiples of 3 or
- *    * 5 below 1024 (excluded), followed by a new line
- *     * Return: 0
- *      */
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
+ */
 int main(void)
 {
 	int c = 0;
