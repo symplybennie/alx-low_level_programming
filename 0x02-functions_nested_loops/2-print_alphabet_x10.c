e58760b5bacb87d8880119e3383c6932c24fe6d1
symply$bennie <46108655+symplybennie@users.noreply.github.com>
2022-04-14 09:33:21 +0100

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,29 +1,26 @@
 #include "main.h"
 
 /**
-   * print_alphabet_x10 - prints alphabet 10 times
-    * Return:void
-     */
+* print_alphabet_x10 - prints alphabet 10 times
+* Return:void
+*/
 
 void print_alphabet_x10(void)
 {
-   char c;
-   int i = 0;
+	char c;
+	int i = 0;
+
 	while (i < 10)
 	{
-           c = 'a';
-             while (c <= 'z')
+		c = 'a';
+		while (c <= 'z')
 		{
-	        	_putchar(c);		
-		        c++;	
+			_putchar(c);
+			c++;
 		}
-			_putchar('\n');
-				i++;
-
-        	}
-
-
+		_putchar('\n');
+		i++;
+	}
 
 }
-
 
