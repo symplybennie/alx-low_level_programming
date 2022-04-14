889c64b4f4ec9b448e075855b21d14824d31ef48
symply$bennie <46108655+symplybennie@users.noreply.github.com>
2022-04-14 10:37:50 +0100

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,61 +1,47 @@
 #include "main.h"
 
 /**
-   * times_table - prints times table
-    * Return:void
-     */
+* times_table - prints times table
+* Return:void
+*/
 
 void times_table(void)
 {
 
 	int a = 0;
-
 	int b;
-
 	int rep;
-
 	while (a <= 9)
-  {
-	   b = 0;
+	{
+		b = 0;
 		while (b <= 9)
-		   {
-	              rep = a * b;
-		      
-		      if (b == 0)
+		{
+			rep = a * b;
+			if (b == 0)
 			{
-  		        	_putchar('0' + rep);
-
+				_putchar('0' + rep);
 			}
-	          	
-		      else if (rep < 10)
-		            {
-		        	_putchar(' ');
- 		
-		        	_putchar('0' + rep);
-		             }
-		
-		       else
-	                {
-
-			   _putchar('0' + rep / 10);
-			
-			   _putchar('0' + rep % 10);
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else
+			{
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
 			}
 
-                        if (b < 9)
-		          {
+			if (b < 9)
+			{
 				_putchar(',');
-
-		        	_putchar(' ');
-
-			  }
-
- 	         	b++;
+				_putchar(' ');
 			}
-		
+			b++;
+		}
 		_putchar('\n');
-
-			a++;
-    }
+		a++;
+	}
 
 }
+
