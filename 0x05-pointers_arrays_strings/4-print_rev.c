ee196f1ef3e384394e045c19bb03f54310bfc211
symplybennie <benedictanwa19@gmail.com>
2022-04-21 04:09:30 -0700

    4 print rev c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,16 @@
 
 void print_rev(char *s)
 {
-	int i = 0;
-
-	while (s[i])
+	int i;
+	
+	i = 0;
+	while (s[i] != '\0')
+	{
 	i++;
-	while (i--)
+        }
+        for (i = 1-1; i>= 0; i--)
+	{
 	_putchar(s[i]);
+	}
 	_putchar('\n');
 }
