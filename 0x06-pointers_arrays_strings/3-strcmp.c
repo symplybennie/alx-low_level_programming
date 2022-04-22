8964aa0ccc6640ddc8c21a17120ad7f13009b8b2
symplybennie <benedictanwa19@gmail.com>
2022-04-22 05:40:28 -0700

    a function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,7 @@
 * Return: 0 if s1 and s2 are equal
 *        negative integer if the stopping character
 *                in @s1 was less than the stopping
-*     character in @s2
+*                character in @s2
 *        positive integer if the stopping character
 *                in @s1 was greater than the stopping
 *                character in @s2
@@ -17,7 +17,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
- int i = 0, r;
+int i = 0, r;
 
 /**
 * iterate through src and
@@ -29,19 +29,19 @@ while (s1[i] != '\0' && s2[i] != '\0')
 * if they differ by a single character
 * don't iterate further
 */
-   if (s1[i] != s2[i])
-   {
+  if (s1[i] != s2[i])
+  {
 /*
 * return the difference between
 * the two characters
 */
-         r = s1[i] - s2[i];
-         break;
-    }
-else
-{
-r = s1[i] - s2[i];
-}
+     r = s1[i] - s2[i];
+     break;
+  }
+  else
+  {
+  r = s1[i] - s2[i];
+  }
 i++;
 }
 return (r);
