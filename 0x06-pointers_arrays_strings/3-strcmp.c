d933a6256d3d5eb37562ea02a8a807d30752a420
symplybennie <benedictanwa19@gmail.com>
2022-04-25 06:25:08 -0700

    Write a function that compares two strings.y

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -29,19 +29,19 @@ while (s1[i] != '\0' && s2[i] != '\0')
 * if they differ by a single character
 * don't iterate further
 */
-  if (s1[i] != s2[i])
-  {
+if (s1[i] != s2[i])
+{
 /*
 * return the difference between
 * the two characters
 */
-     r = s1[i] - s2[i];
-     break;
-  }
-  else
-  {
-  r = s1[i] - s2[i];
-  }
+r = s1[i] - s2[i];
+break;
+}
+else
+{
+r = s1[i] - s2[i];
+}
 i++;
 }
 return (r);
