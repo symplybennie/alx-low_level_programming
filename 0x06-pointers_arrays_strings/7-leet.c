cd9683312b12627117c4a00f8538b1d4b41e1851
symplybennie <benedictanwa19@gmail.com>
2022-04-25 07:13:12 -0700

    Write a function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,15 +15,11 @@ int low_letters[] = {97, 101, 111, 116, 108};
 int upp_letters[] = {65, 69, 79, 84, 76};
 int numbers[] = {52, 51, 48, 55, 49};
 
-/**
-*while (s[c] != '\0')
-*{
-* loop through array value five times
-* to check if the value is equal to
-* any small letter in array sl or
-* upper letter in array ul and if it
-* is replace it with the value in array n
-*/
+/*
+*while (s[c] != '\0') loop through array value
+* five times to check if the value is equal to
+* any small letter in array sl or upper letter in array ul and if it
+* is replace it with the value in array n **/
 while (*(s + count) != '\0')
 {
 for (i = 0; i < 5; i++)
