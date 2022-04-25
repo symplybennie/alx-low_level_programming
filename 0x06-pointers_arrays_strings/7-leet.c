2dc67e18c41617581ff259b952f355374f771a14
symplybennie <benedictanwa19@gmail.com>
2022-04-25 04:26:37 -0700

    a function that encodes a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,29 +10,31 @@
 
 char *leet(char *s)
 {
-int i, c = 0;
-int sl[] = {97, 101, 111, 116, 108};
-int ul[] = {65, 69, 79, 84, 76};
-int n[] = {52, 51, 48, 55, 49};
+int count = 0, i;
+int low_letters[] = {97, 101, 111, 116, 108};
+int upp_letters[] = {65, 69, 79, 84, 76};
+int numbers[] = {52, 51, 48, 55, 49};
 
 /**
-*iterate values in array s
 *while (s[c] != '\0')
-{
+*{
 * loop through array value five times
 * to check if the value is equal to
 * any small letter in array sl or
 * upper letter in array ul and if it
 * is replace it with the value in array n
 */
+while (*(s + count) != '\0')
+{
 for (i = 0; i < 5; i++)
 {
-if (s[c] == sl[i] || s[c] == ul[i])
+if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
 {
-s[c] = n[i];
+*(s + count) = numbers[i];
 break;
 }
 }
-c++;
+count++;
+}
 return (s);
 }
