b2bbdf893c9cd87ae39a3f1ea46e14dcbca7fe75
symplybennie <benedictanwa19@gmail.com>
2022-05-02 23:46:48 -0700

    static_libraries

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strien -length of a string
+*@s: A pointer to an int that will be changed/updated
+*
+*Return: void means our answer is correct
+*/
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		count++;
+	}
+
+	return (count);
+}
