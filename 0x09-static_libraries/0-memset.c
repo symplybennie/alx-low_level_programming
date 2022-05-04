bca49ef0e71c5f700e67d309e968d116621fd6c7
symplybennie <benedictanwa19@gmail.com>
2022-05-04 08:38:57 -0700

    0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *_memset -   fills memory with a constant byte
 *@s: pointer block of memory to fill
@@ -10,11 +11,9 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
-
-/*Delacring FOR*/
 for (i = 0; i < n; i++)
 {
-*(s + i) = b; /*add 1 position s*/
-} /*END FOR*/
+s[i] = b; 
+}
 return (s);
 }
