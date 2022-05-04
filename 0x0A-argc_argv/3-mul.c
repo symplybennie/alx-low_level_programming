f6ffdc557b62309ae260f10cd37393f3fae8712b
symplybennie <benedictanwa19@gmail.com>
2022-05-04 07:56:57 -0700

    restructured

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* main - entry point, multiply 2 arguments
+*
+* @argv: argument vector
+* @argc: argument count
+* Return: 0 if 2 args available, 1 otherwise
+*/
+
+int main(int argc, char **argv)
+{
+int a, b;
+
+if (argc >= 3)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a *b);
+return (0);
+}
+printf("Error\n");
+return (1);
+}
