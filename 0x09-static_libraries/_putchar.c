11dccb4c4939216a0423968e6fc56ef8ca822106
symplybennie <benedictanwa19@gmail.com>
2022-05-04 08:16:33 -0700

    putchar

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -11,9 +11,9 @@
 * Return: On success 1
 *
 * On error, -1 is returned, and errno is set appropriatel
-*/ 
+*/
 
-int _putchar(char c)
+int _putchar(char)
 {
-return (write(1, &c, 1)); 		    
+return (write(1, &c, 1));     
 }
