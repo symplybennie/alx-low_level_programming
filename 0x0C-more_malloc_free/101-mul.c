084e25386fa33489d3aa8ef23d74f4459b69528d
symplybennie <benedictanwa19@gmail.com>
2022-05-10 09:17:53 -0700

    my file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,100 +1,33 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
-* isNum - checks if a string contains a non-digit char
-*@s: string to be evaluated
-*
-*Return: 0 if a non-digit is found, 1 otherwise
+* main - multiplies two positive numbers
+* @argc: n arguments
+* @argv: args
+* Return: int
 */
-int isNum(char *s)
-{
-int i = 0;
 
-while (s[i])
+int main(int argc, char *argv[])
 {
-if (s[i] < '0' || s[i] > '9')
-return (0);
-i++;
-}
-return (i);
-}
+unsigned long mul;
+int i, j;
 
-/*
-* _strlen - returns the length of a string
-* @s: string to evaluate
-*
-* Return: the length of the string**/
-int _strlen(char *s)
+if (argc != 3)
 {
-int i = 0;
-
-while (s[i] != '\0')
+printf("Error\n");
+exit(98); }
+for (i = 1; i < argc; i++)
 {
-i++;
-}
-return (i);
-}
-
-/*
-* errors - handles errors for main
-*/
-void errors(void)
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] > 57 || argv[i][j] < 48)
 {
 printf("Error\n");
 exit(98);
 }
-
-/*
-* main - multiplies two positive numbers
-* @argc: number of arguments
-* @argc: array of arguments
-*
-* Return: always 0 (success)
-*/
-int main(int argc, char *argv[])
-{
-char *s1, *s2;
-int len1, len2, len, i, carry;
-int num1, num2;
-int *result, a = 0;
-
-s1 = argv[1], s2 = argv[2];
-if (argc != 3 || !isNum(s1) || !isNum(s2))
-errors();
-len1 = _strlen(s1);
-len2 = _strlen(s2);
-len = len1 + len2 + 1;
-result = malloc(sizeof(int) * len);
-if (!result)
-return (1);
-for (i = 0; i <= len1 + len2; i++)
-result[i] = 0;
-for (len1 = len1 - 1; len1 >= 0; len1--)
-{
-num2 = s2[len2] - '0';
-carry = 0;
-for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-{
-num2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (num1 * num2);
-result[len1 + len2 + 1] = carry % 10;
-carry /= 10;
 }
-if (carry > 0)
-result[len1 + len2 + 1] += carry;
 }
-for (i = 0; i < len - 1; i++)
-{
-if (result[i])
-a = 1;
-if (a)
-_putchar(result[i] + '0');
-}
-if (!a)
-_putchar('0');
-_putchar('\n');
-free(result);
+mul = atol(argv[1]) *  atol(argv[2]);
+printf("%lu\n", mul);
 return (0);
 }
